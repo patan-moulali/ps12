b84e5bf513cc50a97f31002a9d1a7bd0a2ccbc3d
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 20:52:39 +0530

    Add project files.

---
+++ ps12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+            
+            if (string.IsNullOrEmpty (s));
+            {
+                Console.WriteLine("string is either null or empty");
+                Console.Read();
+            }
+        }
+    }
+}
